f25edc96812680d22da92ddd17301520bad78a26
Cory Christensen <cory.m.christensen@aggiemail.usu.edu>
2015-11-13 17:33:44 -0700

    Creation of Classes - Panel, ChangePanel, and Main class

---
+++ src/Panel.java
@@ -0,0 +1,20 @@
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Panel {
+	JFrame mainFrame;
+	JButton greenButton; //Green Button for Simon Game
+	JButton redButton; // Red ""
+	JButton yellowButton; // Yellow ""
+	JButton blueButton; // Blue ""
+	JButton newGame; // Resets the game
+	JLabel message; // Messages to user
+	JLabel highScore; // Displays High Score
+	
+}
---
+++ src/Simonish.java
@@ -0,0 +1,9 @@
+
+public class Simonish {
+	
+	public static void main(String args[])
+	{
+		
+	}
+	
+}
